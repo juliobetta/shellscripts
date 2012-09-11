6b0191688f3d305b2ac280945e876a2183e10da4
Julio Betta <juliobetta@gmail.com>
2012-09-11 20:33:06 -0300

    mudei o condicional

--- checa_1001/checa_1001.sh
+++ checa_1001/checa_1001.sh
@@ -8,7 +8,7 @@ url="https://vendas.autoviacao1001.com.br/perl/br5.cgi"
 params="ida=soloida&txt_desde=521&txt_hasta=359&fecha=120914"
 response=`curl -d "$params" ${url} | egrep "<td>(2[0-9]+:[0-9]+)"`
 
-if [[ ! -z ${response} ]]; then
+if [ ! -z ${response} ]; then
  ruby sendmail.rb $1 $2 "${url}?${params}"
 else
   touch log.txt
