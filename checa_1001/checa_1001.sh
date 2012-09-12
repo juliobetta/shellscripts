213b036faa86c3b3b1a93a4cec70ff4e08960be6
Julio Betta <juliobetta@gmail.com>
2012-09-12 20:05:55 -0300

    reparado erro ao instalar gmail gem; outras correcoes

--- checa_1001/checa_1001.sh
+++ checa_1001/checa_1001.sh
@@ -2,21 +2,30 @@
 #
 # Checar horario de onibus 1001
 #
-
-# Verificar se ruby esta instalado
-command -v ruby >/dev/null 2>&1 || { echo >&2 "Antes de executar, instale ruby. Abortando."; exit 1; }
-
-# Instalar gmail gem, caso nao esteja
-gmail_gem_installed=`gem list | egrep '^gmail '`
-if [ ! -f "${gmail_gem_installed}" ]; then
-  echo "Instalando gmail gem..."
-  gem install gmail
-fi
-
 if [ ! -f "./stop" ]; then
   now=`date +"%m/%d/%Y -  %T"`
+  
+  # Verificar EMAIL
+  if [ -z $1 ]; then
+    echo "$now : Email nao fornecido. Abortando." >> ./log.txt; exit 1;
+  fi
+
+  if [ -z $2 ]; then
+    echo "$now : Senha nao fornecida. Abortando." >> ./log.txt; exit 1;
+  fi
+  
+  # Verificar se ruby esta instalado
+  command -v ruby >/dev/null 2>&1 || { echo >&2 "Antes de executar, instale ruby. Abortando."; exit 1; }
+
+  # Instalar gmail gem, caso nao esteja
+  gmail_gem_installed=`gem list | egrep '^gmail '`
+  if [ -z "${gmail_gem_installed}" ]; then
+    echo "Instalando gmail gem..."
+    gem install gmail
+  fi
+
   url="https://vendas.autoviacao1001.com.br/perl/br5.cgi"
-  params="ida=soloida&txt_desde=521&txt_hasta=359&fecha=120914"
+  params="ida=soloida&txt_desde=521&txt_hasta=359&fecha=120921"
   response=`curl -d "$params" ${url} | egrep "<td>(2[0-9]+:[0-9]+)"`
 
   if [ ! -z "${response}" ]; then
